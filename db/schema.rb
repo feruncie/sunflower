b13adad91c1e046b95a1458e6aa24a5cd1060d5d
feruncie <fruncie@chicagobooth.edu>
2024-08-12 21:06:15 +0000

    created posts boilerplate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_12_205158) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_12_210530) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text "title"
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
