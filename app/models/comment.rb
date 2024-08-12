baa17366ac349345de77b86977254e4f99cf2787
feruncie <fruncie@chicagobooth.edu>
2024-08-12 21:07:50 +0000

    created comments boilerplate

---
+++ app/models/comment.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :bigint           not null, primary key
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  post_id    :integer
+#  user_id    :integer
+#
+class Comment < ApplicationRecord
+end
